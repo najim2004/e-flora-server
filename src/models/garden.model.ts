9f431bca9028af478be8cdb8243372a85a66efc8
najim2004 <najim10112004@gmail.com>
2025-06-25 12:00:00 +0600

    update: add garden model with schema definition and export

---
+++ src/models/garden.model.ts
@@ -0,0 +1,33 @@
+import { model, models, Schema } from 'mongoose';
+import { IGarden } from '../interfaces/garden.interface';
+
+const gardenSchema = new Schema<IGarden>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    name: String,
+    description: String,
+    location: {
+      latitude: Number,
+      longitude: Number,
+    },
+    size: Number,
+    Weather: {
+      maxTemp: Number,
+      minTemp: Number,
+      humidity: Number,
+      rainfall: Number,
+      windSpeed: Number,
+      dominantWindDirection: String,
+      date: Date,
+    },
+    crops: [{ type: Schema.Types.ObjectId, ref: 'GardenCrop' }],
+    activeCrops: Number,
+    pendingCrops: Number,
+    removedCrops: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Garden = models.Garden || model<IGarden>('Garden', gardenSchema);
